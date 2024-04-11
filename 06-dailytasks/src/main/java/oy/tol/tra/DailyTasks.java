280f0a3562f44d002dd4659657d5659d6e39c5cd
jiao4 <126316383+jiao4@users.noreply.github.com>
2024-04-11 17:17:13 +0800

    Update DailyTasks.java

--- 06-dailytasks/src/main/java/oy/tol/tra/DailyTasks.java
+++ 06-dailytasks/src/main/java/oy/tol/tra/DailyTasks.java
@@ -20,16 +20,16 @@ public class DailyTasks {
 
     private void run() {
         try {
-            dailyTaskQueue = new QueueImplementation<>(); // Create a new queue for daily tasks
+            dailyTaskQueue = new QueueImplementation<>(); 
             readTasks(); // Read the tasks for today
 
             timer = new Timer(); // Create Java Timer object
             timer.scheduleAtFixedRate(new TimerTask() {
                 @Override
                 public void run() {
-                    if (!dailyTaskQueue.isEmpty()) { // Check if there are tasks in the queue
-                        String task = dailyTaskQueue.dequeue(); // Dequeue the task
-                        System.out.println(task); // Print the task
+                    if (!dailyTaskQueue.isEmpty()) { 
+                        String task = dailyTaskQueue.dequeue(); 
+                        System.out.println(task); 
                     } else {
                         timer.cancel(); // Cancel the timer if there are no tasks
                     }
@@ -45,8 +45,8 @@ public class DailyTasks {
         tasks = new String(getClass().getClassLoader().getResourceAsStream("DailyTasks.txt").readAllBytes());
         String[] allTasks = tasks.split("\\r?\\n");
         for (String task : allTasks) {
-            dailyTaskQueue.enqueue(task); // Enqueue the task to the Queue implementation
+            dailyTaskQueue.enqueue(task); 
         }
-        System.out.println("Number of tasks in the queue: " + dailyTaskQueue.size()); // Print the number of tasks
+        System.out.println("Number of tasks in the queue: " + dailyTaskQueue.size()); 
     }
 }
